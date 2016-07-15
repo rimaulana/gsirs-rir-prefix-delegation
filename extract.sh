95a92ece3529024910ef8983d778cef4a135bbd6
rimaulana <rimaulana@outlook.com>
2016-07-16 00:30:20 +0700

    Added download capability

--- extract.sh
+++ extract.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
 
-echo "Hello world!"
+# Getting directory where file resides
+SCRIPT=$(readlink -f $0)
+ROOTDIR=`dirname $SCRIPT`
+
+# Getting passed parameter
+shopt -s extglob
+URL=${1##*( )}
+URL=${URL%%*( )}
+OUTPUT=${2##*( )}
+OUTPUT=${OUTPUT%%*( )}
+shopt -u extglob
+
+# Defining file to temporarily hold data from the url
+TEMPFILE="$ROOTDIR/buffer.temp"
+
+# Checking whether parameter is passed
+if [[ -z "$URL" ]]; then
+	echo "You need to define the link to rir delegation source"
+	exit 1
+fi
+
+if [[ -z "$OUTPUT" ]]; then
+	echo "You need to define the output file"
+	exit 1
+else
+	if [[ -e "$OUTPUT" ]]; then
+		echo "Output file exist, please choose different output file name"
+		exit 1
+	fi
+fi
+
+if [[ -e "$TEMPFILE" ]]; then
+	rm -rf $TEMPFILE
+fi
+
+curl --fail -o $TEMPFILE $URL
